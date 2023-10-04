8f9db7deed7b78508afc88d80406d29de8aecad8
maquintoch <macceluno@hotmail.com>
2023-10-04 09:12:51 +0200

    Added daylireport.yml updated and sen_email.py

---
+++ send_email.py
@@ -0,0 +1,19 @@
+import smtplib
+import ssl
+import os
+
+
+port = 465
+smtp_Server = "smtp.gmail.com"
+UserName = os.environ.get('USER_EMAIL')
+Password = os.environ.get('USER_PASSWORD')
+message = """\
+Subject: GitHub Email Report
+
+This is your daily report
+    """
+
+context = ssl.create_default_context
+with smtplib.SMTP_SSL(smtp_Server, port, context=context) as server:
+    server.login(UserName, Password)
+    server.sendmail(UserName, UserName, message)
